07afd16583a4148e606318e0f66b0de2c8224c65
Vimu-Sama <42619785+Vimu-Sama@users.noreply.github.com>
2025-10-26 09:15:55 +0000

    Completed a simple web server

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Following Error ocurred while parsing-> %v", err)
+	}
+	name := r.FormValue("UserName")
+	address := r.FormValue("Email")
+	fmt.Fprintf(w, "The values were posted successfully")
+	fmt.Printf("Name-> %v\n", name)
+	fmt.Printf("Email address-> %v", address)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Bad Request Made", http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("Hello Page Accessed\n")
+	fmt.Fprintf(w, "This is the Home Page of the local browser created")
+}
 
 func main() {
-	fmt.Printf("Hello World!!\n")
+	fs := http.FileServer(http.Dir("./static"))
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file!")
+	}
+	portNum := os.Getenv("PORT")
+	http.Handle("/", fs)
+	http.HandleFunc("/hello", homeHandler)
+	http.HandleFunc("/form", formHandler)
+	fmt.Printf("The server is running...")
+	if err := http.ListenAndServe(":"+portNum, nil); err != nil {
+		fmt.Printf("There was error in showing the page")
+		log.Fatal(err)
+	}
 }
